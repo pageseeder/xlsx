6197b59a68ffed5ec82edfcca1db0c78f5a8a032
ccabral <ccabral@allette.com.au>
2021-06-30 16:46:09 +1000

    Add read file content.

--- pso-xlsx-core/src/main/java/org/pageseeder/xlsx/util/FileUtils.java
+++ pso-xlsx-core/src/main/java/org/pageseeder/xlsx/util/FileUtils.java
@@ -5,6 +5,9 @@ package org.pageseeder.xlsx.util;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 public class FileUtils {
   /**
@@ -18,4 +21,28 @@ public class FileUtils {
     }
 
   };
+
+  /**
+   * Returns the content of the source file.
+   *
+   * @param source the source
+   * @return the string
+   * @throws IOException Signals that an I/O exception has occurred.
+   */
+  public static String read(File source) throws IOException {
+    return read(source, "UTF-8");
+  }
+
+  /**
+   * Returns the content of the source file.
+   *
+   * @param source the source
+   * @return the string
+   * @throws IOException Signals that an I/O exception has occurred.
+   */
+  public static String read(File source, String charset) throws IOException {
+    Charset encoding = Charset.forName(charset);
+    byte[] encoded = Files.readAllBytes(source.toPath());
+    return new String(encoded, encoding);
+  }
 }
