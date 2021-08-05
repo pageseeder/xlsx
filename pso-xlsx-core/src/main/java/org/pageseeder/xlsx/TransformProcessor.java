2602618841db077c9ffdcf24c742e3951bf6e7a4
adrian <aakaishi@allette.com.au>
2021-08-05 17:23:00 +1000

    Add style parse.

--- pso-xlsx-core/src/main/java/org/pageseeder/xlsx/TransformProcessor.java
+++ pso-xlsx-core/src/main/java/org/pageseeder/xlsx/TransformProcessor.java
@@ -1,15 +1,29 @@
 /*
- *  Copyright (c) 2021 Allette Systems pty. ltd.
+ * Copyright 2021 Allette Systems (Australia)
+ * http://www.allette.com.au
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.pageseeder.xlsx;
 
 import org.pageseeder.xlsx.config.Param;
 import org.pageseeder.xlsx.config.SplitLevel;
 import org.pageseeder.xlsx.config.TransformConfig;
+import org.pageseeder.xlsx.core.Style;
 import org.pageseeder.xlsx.core.WorkBook;
 import org.pageseeder.xlsx.core.WorkSheet;
 import org.pageseeder.xlsx.interim.Interim;
 import org.pageseeder.xlsx.util.*;
+import org.pageseeder.xlsx.xml.StyleHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -180,6 +194,11 @@ public class TransformProcessor {
       for (Relationship r : relationships.forType(Relationship.Type.sharedStrings)) {
         shared = SharedStrings.parse(new File(xlWorkbookFolder, r.target()));
       }
+
+      //Parse styles.xml
+      StyleHandler styleHandler = new StyleHandler();
+      XML.parse(xlStyle, styleHandler, true);
+      Style style = styleHandler.getStyle();
 
       // Parse workbook
       WorkBook workbook = WorkBook.parse(xlWorkbook, relationships);
