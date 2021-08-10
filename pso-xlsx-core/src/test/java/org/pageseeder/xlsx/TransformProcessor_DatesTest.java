7f6f237899ef34979b531175807f9074f12f8c44
Valerie Ku <vku@allette.com.au>
2021-08-10 17:11:11 +1000

    update DatesTest (RichTextTrue) to assert new date and time formats

--- pso-xlsx-core/src/test/java/org/pageseeder/xlsx/TransformProcessor_DatesTest.java
+++ pso-xlsx-core/src/test/java/org/pageseeder/xlsx/TransformProcessor_DatesTest.java
@@ -38,21 +38,21 @@ public class TransformProcessor_DatesTest {
 
       //Default 14
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='2']/col[@ref='A']/text()").isEqualTo("Default 14");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='2']/col[@ref='B']/text()").isEqualTo("01/02/1900");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='2']/col[@ref='C']/text()").isEqualTo("28/02/1900");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='2']/col[@ref='D']/text()").isEqualTo("01/02/2021");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='2']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='2']/col[@ref='C']/text()").isEqualTo("1900/02/28");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='2']/col[@ref='D']/text()").isEqualTo("2021/02/01");
 
       //DD/MM/YYYY
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='3']/col[@ref='A']/text()").isEqualTo("DD/MM/YYYY");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='3']/col[@ref='B']/text()").isEqualTo("01/02/1900");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='3']/col[@ref='C']/text()").isEqualTo("28/02/1900");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='3']/col[@ref='D']/text()").isEqualTo("01/02/2021");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='3']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='3']/col[@ref='C']/text()").isEqualTo("1900/02/28");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='3']/col[@ref='D']/text()").isEqualTo("2021/02/01");
 
       //M/D/YYYY
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='4']/col[@ref='A']/text()").isEqualTo("M/D/YYYY");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='4']/col[@ref='B']/text()").isEqualTo("2/1/1900");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='4']/col[@ref='C']/text()").isEqualTo("2/28/1900");
-      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='4']/col[@ref='D']/text()").isEqualTo("2/1/2021");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='4']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='4']/col[@ref='C']/text()").isEqualTo("1900/02/28");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='4']/col[@ref='D']/text()").isEqualTo("2021/02/01");
 
       //YYYY/MM/DD
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='5']/col[@ref='A']/text()").isEqualTo("YYYY/MM/DD");
@@ -60,10 +60,77 @@ public class TransformProcessor_DatesTest {
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='5']/col[@ref='C']/text()").isEqualTo("1900/02/28");
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='5']/col[@ref='D']/text()").isEqualTo("2021/02/01");
 
-
       //Empty DD/MM/YYYY
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='6']/col[@ref='A']/text()").isEqualTo("Empty");
       assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='6']/col[@ref='B']/text()").isEqualTo("");
+
+      //D/M/YYYY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='7']/col[@ref='A']/text()").isEqualTo("D/M/YYYY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='7']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //D/MM/YY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='8']/col[@ref='A']/text()").isEqualTo("D/MM/YY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='8']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //D/M/YY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='9']/col[@ref='A']/text()").isEqualTo("D/M/YY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='9']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //DD/MM/YY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='10']/col[@ref='A']/text()").isEqualTo("DD/MM/YY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='10']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //DD-MON-YY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='11']/col[@ref='A']/text()").isEqualTo("DD-MON-YY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='11']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //DD-MONTH-YYYY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='12']/col[@ref='A']/text()").isEqualTo("DD-MONTH-YYYY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='12']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //D Month YYYY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='13']/col[@ref='A']/text()").isEqualTo("D Month YYYY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='13']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //MM/DD/YY
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='14']/col[@ref='A']/text()").isEqualTo("MM/DD/YY");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='14']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //YYYY-MM-DD
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='15']/col[@ref='A']/text()").isEqualTo("YYYY-MM-DD");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='15']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //YY-MM-DD
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='16']/col[@ref='A']/text()").isEqualTo("YY-MM-DD");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='16']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //YYYY/MM/DD
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='17']/col[@ref='A']/text()").isEqualTo("YYYY/MM/DD");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='17']/col[@ref='B']/text()").isEqualTo("1900/02/01");
+
+      //D/M/YY hh:mm AM/PM
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='18']/col[@ref='A']/text()").isEqualTo("D/M/YY hh:mm AM/PM");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='18']/col[@ref='B']/text()").isEqualTo("1900/01/10 01:00:00");
+
+      //D/M/YY hh:mm
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='19']/col[@ref='A']/text()").isEqualTo("D/M/YY hh:mm");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='19']/col[@ref='B']/text()").isEqualTo("2021/07/26 00:00:01");
+
+      //hh:mm:ss AM/PM
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='20']/col[@ref='A']/text()").isEqualTo("hh:mm:ss AM/PM");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='20']/col[@ref='B']/text()").isEqualTo("00:01:00");
+
+      //h:mm:ss
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='21']/col[@ref='A']/text()").isEqualTo("h:mm:ss");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='21']/col[@ref='B']/text()").isEqualTo("01:01:01");
+
+      //hh:mm:ss
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='22']/col[@ref='A']/text()").isEqualTo("hh:mm:ss");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='22']/col[@ref='B']/text()").isEqualTo("11:39:00");
+
+      //String formatted as date
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='23']/col[@ref='A']/text()").isEqualTo("String formatted as date");
+      assertThat(interimXML).valueByXPath("/workbook/worksheet/row[@position='23']/col[@ref='B']/text()").isEqualTo("I am a string that wants to be a date");
 
     } catch (IOException e) {
       Assert.fail(e.getMessage());
